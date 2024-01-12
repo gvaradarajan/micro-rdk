c9da663956df762bc1ba0ce96142da1c148157cc
Gautham <gautham@viam.com>
2024-01-12 14:26:21 -0500

    some simple fixes

--- canary/canary_test.py
+++ canary/canary_test.py
@@ -5,13 +5,16 @@ import time
 
 from pymongo import MongoClient
 from typing import Coroutine, Any
-from viam.robot.client import RobotClient
+from viam.robot.client import RobotClient, DialOptions
 from viam.components.board import Board
 
 async def connect(robot_address: str, api_key: str, api_key_id: str) -> Coroutine[Any, Any, RobotClient]:
-    opts = RobotClient.Options.with_api_key(
-      api_key=api_key,
-      api_key_id=api_key_id
+    opts = RobotClient.Options(
+        refresh_interval=0,     	 
+        check_connection_interval=0,
+        attempt_reconnect_interval=0,
+        disable_sessions=True,
+        dial_options=DialOptions.with_api_key(api_key_id=api_key_id,api_key=api_key)
     )
     return await RobotClient.at_address(robot_address, opts)
 
@@ -60,7 +63,9 @@ async def main():
         board_return_value = await board.gpio_pin_by_name("32")
         _ = await board_return_value.get()
         await board_return_value.set(True)
-        _ = await board_return_value.get()
+        value = await board_return_value.get()
+        if not value:
+            raise ValueError("Pin not set to high successfully")
         default_item["board_api_success"] = True
     except Exception as e:
         default_item["error"] = str(e)
